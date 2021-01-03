11febd094bbfef1fd471af6794207d4e26a4894d
Yuriy <xaosmmm@gmail.com>
2021-01-03 14:25:03 +0200

    Added library

---
+++ API ReustorantApp/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+
+namespace API_ReustorantApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IFoodData _foodData;
+        private readonly IOrderData _orderData;
+
+        public OrderController(IFoodData foodData, IOrderData orderData)
+        {
+            _foodData = foodData;
+            _orderData = orderData;
+        }
+    }
+}
